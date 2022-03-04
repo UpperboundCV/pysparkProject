1c0f050e521bf05af784e2e514c90ae25ed91605
up <upperbound.work@gmail.com>
2022-03-05 00:11:18 +0700

    change dataframe helper

--- sparkcore/helper/DataFrameHelper.py
+++ sparkcore/helper/DataFrameHelper.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 
 import pyspark
-from pyspark.sql.functions import col, lit, max, when, md5, concat, lower, to_date, upper, to_timestamp, unix_timestamp
+from pyspark.sql.functions import col, lit, max, when, md5, concat, lower, to_date, upper, to_timestamp, unix_timestamp, \
+    trim
 from pyspark.sql.functions import months_between, coalesce, last_day, date_format, substring, regexp_replace, lower, \
     broadcast
 from pyspark.sql.types import IntegerType, StringType, DateType, TimestampType
@@ -40,8 +41,8 @@ class DataFrameHelper:
         ka_entity_flag = (lower(col(KA_ENTITY)) == 'bay')
         is_ay_entity_null = ((lower(col(AY_ENTITY)).isNull()) | (col(AY_ENTITY) == ' '))
         is_ka_entity_null = ((lower(col(KA_ENTITY)).isNull()) | (col(KA_ENTITY) == ' '))
-        ay_status = (lower(col(AY_STATUS)) == cls.ACTIVE)
-        ka_status = (lower(col(KA_STATUS)) == cls.ACTIVE)
+        ay_status = (lower(trim(col(AY_STATUS))) == cls.ACTIVE)
+        ka_status = (lower(trim(col(KA_STATUS))) == cls.ACTIVE)
         is_ay_status_null = ((lower(col(AY_STATUS)).isNull()) | (col(AY_STATUS) == ' '))
         is_ka_status_null = ((lower(col(KA_STATUS)).isNull()) | (col(KA_STATUS) == ' '))
         return when(ay_entity_flag & ay_status & ka_entity_flag & ka_status, lit('Auto')) \
@@ -52,13 +53,11 @@ class DataFrameHelper:
             .when((ay_entity_flag & ~ay_status) & (ka_entity_flag & ~ka_status), lit(None)) \
             .otherwise(lit('').cast(StringType()))
 
-
     @classmethod
     def add_user_type(cls) -> pyspark.sql.functions.col:
         return when(col('user_login').rlike("\D"), lit('CR')) \
             .when(col('user_login').rlike("\d"), lit('STAFF')) \
             .otherwise(lit('Other'))
-
 
     @classmethod
     def config_type_to_df_type(cls, config_data_type: str) -> Optional[str]:
@@ -73,11 +72,9 @@ class DataFrameHelper:
         else:
             raise TypeError("data type does not exist.")
 
-
     @classmethod
     def convert_as400_data_date_to_yyyyMMdd(cls, data_date_col_name: str) -> pyspark.sql.functions.col:
         return (col(data_date_col_name).cast(IntegerType()) + lit(20000000) - lit(430000)).cast(StringType())
-
 
     @classmethod
     def convert_yyyyMMdd_col_to_spark_date(cls, yyyyMMdd_col: pyspark.sql.functions.col) -> pyspark.sql.functions.col:
@@ -85,7 +82,6 @@ class DataFrameHelper:
         extracted_year_month = concat(extracted_year, concat(substring(yyyyMMdd_col, 5, 2), lit('-')))
         extracted_year_month_day = concat(extracted_year_month, concat(substring(yyyyMMdd_col, 7, 2)))
         return concat(to_date(extracted_year_month_day, "yyyy-MM-dd").cast(StringType()), lit(' 00:00:00'))
-
 
     @classmethod
     def convert_as400_data_date_to_timestamp(cls, df: pyspark.sql.dataframe.DataFrame,
@@ -106,13 +102,11 @@ class DataFrameHelper:
 
         return convert_all_in_group(df, 0)
 
-
     @classmethod
     def data_date_convert(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
                           data_date_col_name: str) -> pyspark.sql.dataframe.DataFrame:
         yyyy_mm_dd_col = (col(data_date_col_name).cast(IntegerType()) + lit(20000000) - lit(430000)).cast(StringType())
         return transaction_df.withColumn(data_date_col_name, to_date(yyyy_mm_dd_col, "yyyyMMdd").cast(StringType()))
-
 
     @classmethod
     def with_entity_code(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -122,13 +116,11 @@ class DataFrameHelper:
         else:
             return transaction_df.withColumn(cls.ENTITY_CODE, lit('BAY'))
 
-
     @classmethod
     def to_entity_dwh(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         return transaction_df.withColumn(cls.ENTITY_CODE, when(col(cls.ENTITY_CODE) == "AYCAL", lit("AY"))
                                          .when(col(cls.ENTITY_CODE) == "BAY", lit("KA"))
                                          .otherwise(None))
-
 
     @classmethod
     def with_entity(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
@@ -137,7 +129,6 @@ class DataFrameHelper:
                                          .when(col(cls.ENTITY_CODE) == "BAY", lit("KA"))
                                          .otherwise(None))
 
-
     @classmethod
     def with_gecid(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         return transaction_df.withColumn("gecid",
@@ -145,11 +136,9 @@ class DataFrameHelper:
                                          .when(col(cls.ENTITY_CODE) == "BAY", lit("52800000"))
                                          .otherwise(None))
 
-
     @classmethod
     def with_company(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         return transaction_df.withColumn("company_code", lit('GECAL'))
-
 
     @classmethod
     def with_account(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -158,12 +147,10 @@ class DataFrameHelper:
                                          when(col(contract_code).isNotNull(), col(contract_code)).otherwise(
                                              None))
 
-
     @classmethod
     def with_short_product_key(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         return transaction_df.withColumn('product_key', when(col('product_code') == 'MC', lit("1234abc"))
                                          .when(col("product_code") == 'HP', lit("567xyz")).otherwise(lit(None)))
-
 
     @classmethod
     def with_broadcast_product_key(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -177,7 +164,6 @@ class DataFrameHelper:
             .select("t.*", col(cls.PRODUCT_KEY))
         return transaction_df_w_product_key
 
-
     @classmethod
     def with_product_key(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
                          look_up_product_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
@@ -189,7 +175,6 @@ class DataFrameHelper:
             .select("t.*", col(cls.PRODUCT_KEY))
         return transaction_df_w_product_key
 
-
     @classmethod
     def with_branch_key(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         return transaction_df \
@@ -197,14 +182,12 @@ class DataFrameHelper:
                         when(col(cls.BRANCH_CODE).isNotNull() & col("gecid").isNotNull(),
                              upper(md5(concat(lower(col("entity")), lower(col(cls.BRANCH_CODE)))))))
 
-
     @classmethod
     def with_account_key(cls, transaction_df: pyspark.sql.dataframe.DataFrame) -> pyspark.sql.dataframe.DataFrame:
         product_company_code = concat(lower(col("product_code")), lower(col("company_code")))
         branch_product_company_code = concat(lower(col("branch_code")), product_company_code)
         account_branch_product_company_code = concat(lower(col(cls.ACCOUNT_CODE)), branch_product_company_code)
         return transaction_df.withColumn("account_key", upper(md5(account_branch_product_company_code)))
-
 
     @classmethod
     def with_all_keys(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -214,11 +197,9 @@ class DataFrameHelper:
         transaction_df_w_account_key = cls.with_account_key(transaction_df_w_branch_key)
         return transaction_df_w_account_key
 
-
     @classmethod
     def to_ptn_month_key(cls, yyyy_mm_dd_col_name: str) -> pyspark.sql.functions.col:
         return substring(regexp_replace(yyyy_mm_dd_col_name, '-', ''), 0, 6)
-
 
     @classmethod
     def update_insert_snap_monthly_to_table(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -280,7 +261,6 @@ class DataFrameHelper:
                     .selectExpr(snap_monthly_df_cols)
                 writed_df.write.format("orc").insertInto(snap_month_table, overwrite=True)
                 print(f"[1/1] end: update on month key = {target_month_key}")
-
 
     @classmethod
     def update_insert_status_snap_monthly_to_table(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
@@ -360,7 +340,6 @@ class DataFrameHelper:
                     .write.insertInto(snap_month_table, overwrite=True)
                 print(f"end: month key = {target_month_key}")
 
-
     @classmethod
     def update_insert_status_snap_monthly(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
                                           snap_monthly_df: pyspark.sql.dataframe.DataFrame,
@@ -421,13 +400,11 @@ class DataFrameHelper:
 
                 return updated_df  # non_target_month_key_df.unionByName(updated_df)
 
-
     @classmethod
     def does_month_exist(cls, process_date: str, snap_monthly_df: pyspark.sql.dataframe.DataFrame,
                          data_date_col_name: str) -> bool:
         return snap_monthly_df.where(
             months_between(last_day(col(data_date_col_name)), last_day(lit(process_date))) == 0).count() > 0
-
 
     @classmethod
     def find_month_key_of_process_date(cls, process_date: str, snap_monthly_df: pyspark.sql.dataframe.DataFrame,
@@ -436,12 +413,10 @@ class DataFrameHelper:
             snap_monthly_df.where(months_between(last_day(col(data_date_col_name)), last_day(lit(process_date))) == 0) \
                 .select(max(cls.MONTH_KEY)).first()[0]
 
-
     @classmethod
     def get_month_keys(cls, snap_monthly_df: pyspark.sql.dataframe.DataFrame) -> List[int]:
         return [int(collect_monthkey[cls.MONTH_KEY]) for collect_monthkey in
                 snap_monthly_df.select(col(cls.MONTH_KEY)).collect()]
-
 
     @classmethod
     def update_month_key_zero(cls, snap_monthly_df: pyspark.sql.dataframe.DataFrame,
@@ -455,10 +430,9 @@ class DataFrameHelper:
             .where(col("year").cast(IntegerType()) == int(zero_month_key_last_date.year)) \
             .select(max(cls.MONTH_KEY)).first()[0]
         print(f"month key: {month_key}")
-        return snap_monthly_df.where(col(cls.MONTH_KEY) == 0)\
+        return snap_monthly_df.where(col(cls.MONTH_KEY) == 0) \
             .withColumn(cls.MONTH_KEY, lit(month_key)) \
             .withColumn(cls.PTN_MONTH_KEY, cls.to_ptn_month_key(data_date_col_name))
-
 
     @classmethod
     def update_insert(cls, transaction_df: pyspark.sql.dataframe.DataFrame,
